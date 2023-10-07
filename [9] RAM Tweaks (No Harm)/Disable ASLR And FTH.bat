d32c0d308ec7a936cbcaaaccc840b5875ac489b4
vedant <134783905+v3dqnt@users.noreply.github.com>
2023-10-07 22:35:51 +0530

    Delete [9] RAM Tweaks (No Harm) directory

--- [9] RAM Tweaks (No Harm)/Disable ASLR And FTH.bat
+++
@@ -1,8 +0,0 @@
-echo Disabling Address Space Layout Randomization
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v "MoveImages" /t REG_DWORD /d "0" /f >> APB_Log.txt
-timeout /t 1 /nobreak > NUL
-
-echo Disabling Fault Tolerant Heap
-reg add "HKLM\SOFTWARE\Microsoft\FTH" /v "Enabled" /t REG_DWORD /d "0" /f >> APB_Log.txt
-timeout /t 1 /nobreak > NUL
-
